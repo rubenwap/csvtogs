4a92d630477072ffa1230b4ba5809512a53be9e9
Ruben Sanchez <rubensanchezgarcia@gmail.com>
2018-02-18 15:09:13 +0100

    Added interface length test

--- csv_test.go
+++ csv_test.go
@@ -11,4 +11,8 @@ func TestCsv(t *testing.T) {
 	if actual != expected {
 		t.Errorf("Test failed, expected: '%s', got:  '%s'", expected, actual)
 	}
+	
+	if len(prepare("testdata/test.csv")) < 1 {
+		t.Errorf("The resulting interface has a length of %d", len(prepare("testdata/test.csv")))
+	}
 }
